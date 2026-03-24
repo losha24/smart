1361f5f9b59a022bd1f601e2a7066a3f0aa26c87
losha24 <losha24@gmail.com>
2026-03-24 20:42:56 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,53 +1,12 @@
-async function checkUpdate(){
-
-let res=await fetch("version.json?"+Date.now())
-
-let data=await res.json()
-
-let current=localStorage.getItem("appVersion") || "1.1.0"
-
-if(data.version!==current){
-
-alert("יש גרסה חדשה מתבצע עדכון")
-
-await clearAllCache()
-
-localStorage.setItem("appVersion",data.version)
-
-location.reload(true)
-
-}else{
-
-alert("האפליקציה מעודכנת")
-
-}
-
-}
-
-async function clearAllCache(){
-
-if("caches" in window){
-
-let names=await caches.keys()
-
-for(let name of names){
-
-await caches.delete(name)
-
-}
-
-}
-
-if("serviceWorker" in navigator){
-
-let regs=await navigator.serviceWorker.getRegistrations()
-
-for(let reg of regs){
-
-reg.unregister()
-
-}
-
-}
-
+function manualUpdate(){
+    if(confirm("להוריד גרסה חדשה? הנתונים הקיימים יישמרו.")){
+        fetch('version.json')
+        .then(r=>r.json())
+        .then(v=>{
+            if(v.version!=="1.4.0"){
+                alert(`גרסה חדשה זמינה: ${v.version}`);
+                location.reload(true);
+            } else alert("אתה כבר בגרסה העדכנית");
+        });
+    }
 }
