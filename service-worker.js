042fd9a335d39c33cfe9ac8c38fb70faa6b8d3ad
losha24 <losha24@gmail.com>
2026-03-25 08:04:36 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const cacheName="smartMoney-v1";
-const assets=["./","./index.html","./style.css","./app.js","./loan.js","./mortgage.js","./admin.js","./update.js","./storage.js"];
-
-self.addEventListener("install",e=>{ e.waitUntil(caches.open(cacheName).then(c=>c.addAll(assets))) });
-self.addEventListener("fetch",e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
