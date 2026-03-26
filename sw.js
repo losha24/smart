688a126e9ba83bd1fe630e9ac189fe55f5a19896
losha24 <losha24@gmail.com>
2026-03-27 00:56:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const CACHE="smartmoney-v3.1"
-const urls=["./","./index.html","./style.css","./game.js","./logo.png"]
+const CACHE = "smartmoney-v3.1.2";
+const urls = ["./", "./index.html", "./style.css", "./game.js", "./logo.png"];
 
-self.addEventListener("install",e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(urls)))
-})
+self.addEventListener("install", e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls)));
+    self.skipWaiting(); // גורם לגרסה החדשה להיכנס לתוקף מיד
+});
 
-self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
-})
+self.addEventListener("activate", e => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(k => { if(k !== CACHE) return caches.delete(k); })
+        ))
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+
+// האזנה להודעת עדכון מהאפליקציה
+self.addEventListener("message", e => {
+    if (e.data === "SKIP_WAITING") self.skipWaiting();
+});
