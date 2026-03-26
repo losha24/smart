419bd41ff5932e3dfcdaf89dc90c5cea6d04f3bc
losha24 <losha24@gmail.com>
2026-03-26 21:32:49 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,14 @@
-// קוד PWA service worker
-console.log('service worker loaded');
+self.addEventListener('install', e=>{
+    e.waitUntil(
+        caches.open('smartmoney-v2').then(cache=>{
+            return cache.addAll([
+                '/index.html','/style.css','/app.js','/economy.js','/bank.js','/market.js','/tasks.js','/logo.png'
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', e=>{
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
+});
