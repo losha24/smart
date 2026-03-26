e902b899f2d0b8fe3995203fb251dfe9584f74eb
losha24 <losha24@gmail.com>
2026-03-26 18:31:16 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,13 +3,3 @@ self.addEventListener("install",e=>{
 self.skipWaiting()
 
 })
-
-self.addEventListener("fetch",event=>{
-
-event.respondWith(
-
-fetch(event.request).catch(()=>caches.match(event.request))
-
-)
-
-})
