59b3e0634ee659a908d9517007dd09dfaaea53bc
losha24 <losha24@gmail.com>
2026-03-26 17:40:41 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener("install",e=>{
+
+self.skipWaiting()
+
+})
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+fetch(event.request).catch(()=>caches.match(event.request))
+
+)
+
+})
