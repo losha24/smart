3ecc8f25f681d8fce80538f8bbd412fede5261c9
losha24 <losha24@gmail.com>
2026-03-26 23:11:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,55 +1,32 @@
-const CACHE_NAME = "smartmoney-cache-v2";
-const urlsToCache = [
-  "./",
-  "index.html",
-  "style.css",
-  "app.js",
-  "economy.js",
-  "market.js",
-  "tasks.js",
-  "manifest.json",
-  "logo.png",
-  "version.json"
+const CACHE_NAME = "smartmoney-v2";
+
+const urls = [
+"./",
+"index.html",
+"style.css",
+"app.js",
+"economy.js",
+"market.js",
+"tasks.js",
+"manifest.json",
+"logo.png",
+"version.json"
 ];
 
-// התקנה
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", e => {
+
+e.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urls))
+);
+
 });
 
-// הפעלה
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
+self.addEventListener("fetch", e => {
 
-// טעינת קבצים
-self.addEventListener("fetch", event => {
-
-  if (event.request.url.includes("version.json")) {
-    event.respondWith(fetch(event.request));
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+e.respondWith(
+caches.match(e.request)
+.then(res => res || fetch(e.request))
+);
 
 });
