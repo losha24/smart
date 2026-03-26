043fe3f030ebbe7e6bff7549485448a868e80021
losha24 <losha24@gmail.com>
2026-03-27 00:15:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,10 @@
-const CACHE="smartmoney-v3"
-
-const urls=[
-"./",
-"./index.html",
-"./style.css",
-"./game.js",
-"./logo.png"
-]
+const CACHE="smartmoney-v3.1"
+const urls=["./","./index.html","./style.css","./game.js","./logo.png"]
 
 self.addEventListener("install",e=>{
-
-e.waitUntil(
-
-caches.open(CACHE).then(c=>c.addAll(urls))
-
-)
-
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(urls)))
 })
 
 self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request).then(r=>r||fetch(e.request))
-
-)
-
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
 })
