381b2d945647e5b37aca72c88a564d55e39b7a03
losha24 <losha24@gmail.com>
2026-03-27 01:29:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,36 @@
-const CACHE = "smartmoney-v3.1.2";
-const urls = ["./", "./index.html", "./style.css", "./game.js", "./logo.png"];
+const CACHE_NAME = "smartmoney-v3.1.6";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json",
+  "./logo.png"
+];
 
-self.addEventListener("install", e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls)));
-    self.skipWaiting(); // גורם לגרסה החדשה להיכנס לתוקף מיד
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting(); 
 });
 
-self.addEventListener("activate", e => {
-    e.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(k => { if(k !== CACHE) return caches.delete(k); })
-        ))
-    );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache); 
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
-
-// האזנה להודעת עדכון מהאפליקציה
-self.addEventListener("message", e => {
-    if (e.data === "SKIP_WAITING") self.skipWaiting();
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
