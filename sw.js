034494ed0b2566f41be7bcfb5b140a28fd46cbf4
losha24 <losha24@gmail.com>
2026-03-27 17:01:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,38 @@
-const CACHE_NAME = "smartmoney-v4.4.2";
-const urlsToCache = ["./", "./index.html", "./style.css", "./game.js", "./manifest.json", "./logo.png"];
+const CACHE_NAME = 'smart-money-v4.3.5';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/game.js',
+  '/manifest.json',
+  '/logo.png'
+];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(urlsToCache)));
-  self.skipWaiting();
+// התקנה ושמירת קבצים במטמון
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => {
-    if (k !== CACHE_NAME) return caches.delete(k);
-  }))));
+// הפעלה וניקוי מטמון ישן
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+// שליפת קבצים מהמטמון כשאין אינטרנט
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
