03063387dc7926dc5a4dc25c470ff51688f36781
losha24 <losha24@gmail.com>
2026-03-27 03:10:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,17 @@
-const CACHE_NAME = "smartmoney-v3.1.6";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./game.js",
-  "./manifest.json",
-  "./logo.png"
-];
+const CACHE_NAME = "smartmoney-v3.2.1";
+const urlsToCache = ["./", "./index.html", "./style.css", "./game.js", "./manifest.json", "./logo.png"];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting(); 
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(urlsToCache)));
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache); 
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => {
+    if (k !== CACHE_NAME) return caches.delete(k);
+  }))));
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
