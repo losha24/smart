8456659393249b1d2080a7a6789947ccf22048f2
losha24 <losha24@gmail.com>
2026-03-27 03:16:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "smartmoney-v3.2.1";
+const CACHE_NAME = "smartmoney-v3.2.2";
 const urlsToCache = ["./", "./index.html", "./style.css", "./game.js", "./manifest.json", "./logo.png"];
 
 self.addEventListener("install", (e) => {
