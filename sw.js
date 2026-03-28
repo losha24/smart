72e9363dc14ea4c2b321b926112c5d37f72c30fb
losha24 <losha24@gmail.com>
2026-03-28 09:11:31 +0300

    Update sw.js5.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'smart-money-v5.1.1';
+const cacheName = 'smart-money-v5.2.0';
 const assets = [
   './',
   './index.html',
@@ -11,19 +11,17 @@ const assets = [
   './js/activities.js'
 ];
 
-// התקנה ושמירת קבצים בזיכרון
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  self.skipWaiting(); // גורם לגרסה החדשה להיכנס לתוקף מיד
 });
 
-// ניקוי גרסאות ישנות מהזיכרון בעת עדכון
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => {
     return Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)));
   }));
 });
 
-// שליפת קבצים מהזיכרון גם ללא אינטרנט
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
