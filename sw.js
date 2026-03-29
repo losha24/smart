2e3339962d6349f760521267705875cd2da81a22
losha24 <losha24@gmail.com>
2026-03-29 11:22:48 +0300

    Update sw.js577..

--- sw.js
+++ sw.js
@@ -1,27 +1,45 @@
+/* Smart Money Pro - sw.js - v5.7.7 */
+
 const cacheName = 'smart-money-v5.7.7';
 const assets = [
-  './',
-  './index.html',
-  './style.css',
-  './logo.png',
-  './manifest.json',
-  './js/core.js',
-  './js/ui.js',
-  './js/economy.js',
-  './js/activities.js'
+  'index.html',
+  'style.css',
+  'logo.png',
+  'manifest.json',
+  'js/core.js',
+  'js/ui.js',
+  'js/economy.js',
+  'js/activities.js'
 ];
 
+// התקנה ושמירת קבצים בקאש
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
-  self.skipWaiting(); // גורם לגרסה החדשה להיכנס לתוקף מיד
+  self.skipWaiting(); 
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('SW: Caching Assets');
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// ניקוי גרסאות ישנות מהזיכרון
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => {
-    return Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)));
-  }));
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
+// ניהול בקשות - אסטרטגיית "רשת קודם" לשיפור עדכונים
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
 });
